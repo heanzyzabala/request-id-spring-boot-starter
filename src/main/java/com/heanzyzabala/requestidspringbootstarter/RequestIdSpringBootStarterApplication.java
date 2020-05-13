8593a7fab8402c1a194e800cdcc9d551ef81d049
Heanzy Zabala <heanzy.zabala@gmail.com>
2020-05-13 22:14:48 +0800

    Initial commit

---
+++ src/main/java/com/heanzyzabala/requestidspringbootstarter/RequestIdSpringBootStarterApplication.java
@@ -0,0 +1,13 @@
+package com.heanzyzabala.requestidspringbootstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RequestIdSpringBootStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RequestIdSpringBootStarterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/heanzyzabala/requestidspringbootstarter/RequestIdSpringBootStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heanzyzabala.requestidspringbootstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RequestIdSpringBootStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
